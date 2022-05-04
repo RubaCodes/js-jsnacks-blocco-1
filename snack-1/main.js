0da880514374d1eae46f3feef927845fe7cfb225
rubacodes <rubacodes@gmail.com>
2022-05-04 11:36:38 +0200

    print del numero  maggiore

--- snack-1/main.js
+++ snack-1/main.js
@@ -4,9 +4,9 @@ const secondNumber = Number(prompt('inserisci secondo nummero:'));
 console.log(firstNumber ,secondNumber);
 
 if(firstNumber > secondNumber){
-    console.log('primo numero maggiore del secondo');
+    console.log('primo numero maggiore del secondo ' + firstNumber);
 } else if(firstNumber < secondNumber){
-    console.log('primo numero minore del secondo');
+    console.log('primo numero minore del secondo '+ secondNumber);
 }else {
     console.log('i due numeri sono uguali');
 }
