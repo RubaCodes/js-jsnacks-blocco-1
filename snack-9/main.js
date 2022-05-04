d49d2b02cb3b706862a7a5288a82e22ed467c405
rubacodes <rubacodes@gmail.com>
2022-05-04 18:25:29 +0200

    calcolo offset e verifica quale e' piu lungo

--- snack-9/main.js
+++ snack-9/main.js
@@ -1,4 +1,25 @@
 //Crea due array che hanno un numero di elementi diversi.  Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
-
 const nomi = ['Pippo', 'Pluto', 'Paperino'];
-const cognomi =['Bianco', 'Verde', 'Rosso','Blu','Giallo'];
+const cognomi = ['Bianco', 'Verde', 'Rosso', 'Blu', 'Giallo'];
+
+let firstIsLonger = false;
+let offset;
+if (nomi.length > cognomi.length) {
+    offset = nomi.length - cognomi.length;
+    firstIsLonger = true;
+} else {
+    offset = cognomi.length - nomi.length;
+}
+ 
+console.log(offset, firstIsLonger);
+
+
+//  for (let i = 0; i <  nomi.length + offset; i++) {
+//      if(firstIsLonger){
+//          nomi.push(cognomi[i]);
+//      }else{
+//          cognomi.push(nomi[i]);
+//      }
+//  }
+
+//  console.log(nomi);
