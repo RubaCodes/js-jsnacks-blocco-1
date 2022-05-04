d8ab5831195a92a84ee8ac179fe9d5223cc112e1
rubacodes <rubacodes@gmail.com>
2022-05-04 18:57:28 +0200

    push finale con slice,senza cicli

--- snack-9/main.js
+++ snack-9/main.js
@@ -1,25 +1,22 @@
 //Crea due array che hanno un numero di elementi diversi.  Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
 const nomi = ['Pippo', 'Pluto', 'Paperino'];
-const cognomi = ['Bianco', 'Verde', 'Rosso', 'Blu', 'Giallo'];
+const colori = ['Bianco', 'Verde', 'Rosso', 'Blu', 'Giallo'];
 
+let newArray = [];
 let firstIsLonger = false;
 let offset;
-if (nomi.length > cognomi.length) {
-    offset = nomi.length - cognomi.length;
+if (nomi.length > colori.length) {
+    offset = nomi.length - colori.length;
     firstIsLonger = true;
 } else {
-    offset = cognomi.length - nomi.length;
+    offset = colori.length - nomi.length;
 }
  
 console.log(offset, firstIsLonger);
 
-
-//  for (let i = 0; i <  nomi.length + offset; i++) {
-//      if(firstIsLonger){
-//          nomi.push(cognomi[i]);
-//      }else{
-//          cognomi.push(nomi[i]);
-//      }
-//  }
-
-//  console.log(nomi);
+if(firstIsLonger){
+    newArray = [...colori,...nomi.slice(0,offset)];
+}else{
+    newArray = [...nomi,...colori.slice(0,offset)];
+}
+console.log(newArray);
