f534d001ff9e766982d12ea1daedf9989dcc3da6
rubacodes <rubacodes@gmail.com>
2022-05-04 16:56:22 +0200

    aggiunoto i risultati in una lista

--- snack-7/main.js
+++ snack-7/main.js
@@ -2,10 +2,14 @@
 
 const nomi = ['Pippo','Paperino', 'Pluto'];
 const cognomi = ['Rossi','Verdi','Bianchi'];
+
+let randomName = [];
+for(let i = 0; i < 3;i++){
 //calcolo index random 0-2
 const indexNomi = Math.floor(Math.random() * 3);
 const indexCognomi = Math.floor(Math.random() * 3);
 //chain stringa
-const randomName =  `${nomi[indexNomi]} ${cognomi[indexCognomi]}`;
+randomName.push(`${nomi[indexNomi]} ${cognomi[indexCognomi]}`);
+}
 // log nome
 console.log(randomName);
