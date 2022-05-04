32fc86194ad36056eba68d48113e804a48bdaeb2
rubacodes <rubacodes@gmail.com>
2022-05-04 11:47:24 +0200

    push finale

--- snack-2/main.js
+++ snack-2/main.js
@@ -1,2 +1,13 @@
 // L’utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga.
+
+const firstWord = prompt('inserisci prima parola:');
+const secondWord = prompt('inserisci seconda parola:');
+
+if (firstWord.length < secondWord.length) {
+    console.log(firstWord, secondWord);
+}else if (firstWord.length > secondWord.length) {
+    console.log(secondWord, firstWord);
+}else {
+    console.log(`le due parole hanno la stessa lunghezza ${firstWord} ${secondWord}`);
+}
