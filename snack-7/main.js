7beccffdeeb9236aeb73afb235a8975fca081994
rubacodes <rubacodes@gmail.com>
2022-05-04 16:57:40 +0200

    sistemata indentazione

--- snack-7/main.js
+++ snack-7/main.js
@@ -1,15 +1,15 @@
 //Generatore di "nomi cognomi" casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
 
-const nomi = ['Pippo','Paperino', 'Pluto'];
-const cognomi = ['Rossi','Verdi','Bianchi'];
+const nomi = ['Pippo', 'Paperino', 'Pluto'];
+const cognomi = ['Rossi', 'Verdi', 'Bianchi'];
 
 let randomName = [];
-for(let i = 0; i < 3;i++){
-//calcolo index random 0-2
-const indexNomi = Math.floor(Math.random() * 3);
-const indexCognomi = Math.floor(Math.random() * 3);
-//chain stringa
-randomName.push(`${nomi[indexNomi]} ${cognomi[indexCognomi]}`);
+for (let i = 0; i < 3; i++) {
+    //calcolo index random 0-2
+    const indexNomi = Math.floor(Math.random() * 3);
+    const indexCognomi = Math.floor(Math.random() * 3);
+    //chain stringa
+    randomName.push(`${nomi[indexNomi]} ${cognomi[indexCognomi]}`);
 }
 // log nome
 console.log(randomName);
