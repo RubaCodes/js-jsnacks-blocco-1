9e2b0b2c0dbaa59e7aab82e26626f9b17c3cd899
rubacodes <rubacodes@gmail.com>
2022-05-04 17:29:27 +0200

    push finale

--- snack-8/main.js
+++ snack-8/main.js
@@ -1,4 +1,13 @@
 //Snack 8
 //Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione dispari
-const number = [1,2,3,4,5,6,7,8,9,10];
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+let sum = 0;
+for (let i = 0; i < number.length; i++) {
+    if(i%2 ==0){
+    sum += number[i];
+    }
+}
+
+console.log(sum);
 
