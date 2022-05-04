8e5c8e8de2caf9385466cb83b8b6cbfa30e30ccd
rubacodes <rubacodes@gmail.com>
2022-05-04 19:09:46 +0200

    refactoring

--- snack-9/main.js
+++ snack-9/main.js
@@ -1,24 +1,20 @@
 //Crea due array che hanno un numero di elementi diversi.  Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
 const nomi = ['Pippo', 'Pluto', 'Paperino'];
-const colori = ['Bianco', 'Verde', 'Rosso', 'Blu', 'Giallo'];
+const colori = ['Bianco', 'Verde', 'Rosso', 'Giallo', 'Arancione'];
 
 //const colori = ['Bianco', 'Verde'];
 
 let newArray = [];
-let firstIsLonger = false;
+let firstIsLonger;
 let offset;
 if (nomi.length > colori.length) {
     offset = nomi.length - colori.length;
-    firstIsLonger = true;
-} else {
-    offset = colori.length - nomi.length;
-}
- 
-console.log(offset, firstIsLonger);
-
-if(firstIsLonger){
     newArray = [...colori,...nomi.slice(0,offset)];
-}else{
+} else if(nomi.length < colori.length) {
+    offset = colori.length - nomi.length;
     newArray = [...nomi,...colori.slice(0,offset)];
+}else{
+    console.log('Le due stringhe sono uguali, nessuna copia');
+    offset = 0;
 }
 console.log(newArray);
