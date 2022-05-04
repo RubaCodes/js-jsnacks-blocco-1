15686ecc10f200675e465b469b8b93fb3047b05c
rubacodes <rubacodes@gmail.com>
2022-05-04 16:23:37 +0200

    push finale

---
+++ snack-6/main.js
@@ -0,0 +1,11 @@
+//acquisisci numero2
+let number = Number(prompt('Inserisci numero'));
+//condizione logica
+
+//se pari stampa
+if( number % 2 == 0){
+    console.log(number);
+}else{
+    //preincremento
+    console.log(++number);
+}
